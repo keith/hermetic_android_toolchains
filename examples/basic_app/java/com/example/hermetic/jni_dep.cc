8cfb31b04ceeab460c5567def37f4a423cfb9a37
Keith Smiley <keithbsmiley@gmail.com>
2026-06-22 15:30:38 -0700

    Include something through ndk/sources
    This is kept for backwards compat for headers, but we don't want to
    compile the source files twice.

--- examples/basic_app/java/com/example/hermetic/jni_dep.cc
+++ examples/basic_app/java/com/example/hermetic/jni_dep.cc
@@ -1,6 +1,6 @@
 #include "jni_dep.h"
 
-#include "sources/android/cpufeatures/cpu-features.h"
+#include "ndk/sources/android/cpufeatures/cpu-features.h"
 #include "sources/android/native_app_glue/android_native_app_glue.h"
 
 int add_from_dep(int left, int right) {
