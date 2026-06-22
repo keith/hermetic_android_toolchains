bb487de07223e3d90f25a672df647920797e3fc5
Keith Smiley <keithbsmiley@gmail.com>
2026-06-22 12:51:25 -0700

    Add ndk deps to tests

--- examples/basic_app/java/com/example/hermetic/jni_dep.cc
+++ examples/basic_app/java/com/example/hermetic/jni_dep.cc
@@ -1,5 +1,11 @@
 #include "jni_dep.h"
 
+#include "sources/android/cpufeatures/cpu-features.h"
+#include "sources/android/native_app_glue/android_native_app_glue.h"
+
 int add_from_dep(int left, int right) {
+  static_cast<void>(android_getCpuCount);
+  static_cast<void>(app_dummy);
+
   return left + right;
 }
