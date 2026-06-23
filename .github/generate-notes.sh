f93f0bd5c9ba02bca0e3633e847dc932fd3501a8
Keith Smiley <keithbsmiley@gmail.com>
2026-06-23 10:38:36 -0700

    Improve release notes (#56)

--- .github/generate-notes.sh
+++ .github/generate-notes.sh
@@ -5,19 +5,18 @@ set -euo pipefail
 readonly new_version=$1
 
 cat <<EOF
-Setup docs: https://github.com/keith/hermetic_android_toolchains#usage
-
 ### MODULE.bazel Snippet
 
 \`\`\`bzl
 bazel_dep(name = "hermetic_android_toolchains", version = "$new_version")
+bazel_dep(name = "rules_android", version = "0.7.3")
 
 android = use_extension("@hermetic_android_toolchains//:extensions.bzl", "android")
 android.sdk(
-    version = "35",
-    build_tools_version = "35.0.0",
+    build_tools_version = "37.0.0",
+    version = "37.0",
 )
-android.ndk(version = "r25c")
+android.ndk(version = "r29")
 use_repo(android, "androidsdk", "androidndk")
 
 # Make @rules_android's @androidsdk labels resolve to the hermetic SDK.
@@ -26,4 +25,6 @@ override_repo(rules_android_sdk, "androidsdk")
 
 register_toolchains("@androidndk//:all", "@androidsdk//:all")
 \`\`\`
+
+Setup docs: https://github.com/keith/hermetic_android_toolchains#usage
 EOF
